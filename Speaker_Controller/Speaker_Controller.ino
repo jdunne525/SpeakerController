e739e13944e7fa62bcf094935890f738aa20700b
jdunne <jdunne525@hotmail.com>
2020-05-09 20:49:10 -0500

    X0.m.3 Added speaker control I/O and timing.  presently just using test timing on the led..
    _Branch:master

--- Speaker_Controller/Speaker_Controller.ino
+++ Speaker_Controller/Speaker_Controller.ino
@@ -1,3 +1,5 @@
+#include <timer.h>      //global:  Ticker systick;  setup:  systick.attach(1, Systick1S);  callback: Systick1S.    (No loop handler needed)
+
 /*
  * This is a basic example on how to use Espalexa and its device declaration methods.
  */ 
@@ -13,6 +15,12 @@
 //#define MY_WIFI_SSID  "My Wifi SSID"
 //#define MY_WIFI_PASS  "My Wifi Password"
 
+#define   SPEAKER_MAX_ON_TIME     (8*60*60)   //8 hours in seconds
+
+const int speakerPin = 15; 
+const int ledPin = LED_BUILTIN;
+
+
 Ticker systick;
 
 // prototypes
@@ -26,6 +34,8 @@ const char* password = MY_WIFI_PASS;       // your network password
 
 boolean wifiConnected = false;
 
+uint32_t SpeakersOnDuration = 0;
+
 Espalexa espalexa;
 
 EspalexaDevice* speakersdevice; //this is optional
@@ -37,8 +47,8 @@ void setup()
   Serial.println("Speaker controller Started.");
 
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  
+  pinMode(ledPin, OUTPUT);
+
   // Initialise wifi connection
   wifiConnected = connectWifi();
 
@@ -59,8 +69,9 @@ void setup()
   }
 
   //systick.attach(0.1, Systick100mS);
-  systick.attach(1, Systick1S);
-  
+  systick.attach(1, Systick1S);         //ticker.h
+
+  //timer.every(100, TimerCallback);      //timer.h
 }
 
 //void SetSpeakers(uint8_t brightness) {
@@ -68,8 +79,24 @@ void setup()
 //  //SpeakersChanged(brightness);
 //}
 
+//bool TimerCallback(void) {
+//  return true;    //must return true to keep the timer alive
+//}
+
 void  Systick1S() {
   //keep track of how long the speakers have been on and turn them off when its time...
+
+  if (digitalRead(speakerPin)) {
+    SpeakersOnDuration++;
+    if (SpeakersOnDuration >= SPEAKER_MAX_ON_TIME) {
+      SpeakersOnDuration = 0;
+      TurnSpeakersOff();
+    }
+  }
+
+  //toggle the LED:
+  if (digitalRead(ledPin)) digitalWrite(ledPin, LOW);
+  else digitalWrite(ledPin, HIGH);
   
 }
 
@@ -79,22 +106,28 @@ void loop()
    delay(1);
 }
 
-//our callback functions
+//Alexa callback function:
 void SpeakersChanged(uint8_t brightness) {
-    Serial.print("SpeakersChanged changed to ");
-    
-    //do what you need to do here
-
-    //EXAMPLE
     if (brightness) {
-      Serial.print("ON, brightness ");
-      Serial.println(brightness);
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      TurnSpeakersOn();
     }
     else  {
-      Serial.println("OFF");
-      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+      TurnSpeakersOff();
     }
+}
+
+void TurnSpeakersOn(void) {
+  SpeakersOnDuration = 0;
+  Serial.println("speakers turned ON");
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(speakerPin, HIGH);
+}
+
+void TurnSpeakersOff(void) {
+  SpeakersOnDuration = 0;
+  Serial.println("speakers turned OFF");
+  digitalWrite(ledPin, LOW);
+  digitalWrite(speakerPin, LOW);
 }
 
 // connect to wifi – returns true if successful or false if not
